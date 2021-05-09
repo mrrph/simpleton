92c929849a741582f18d45f740a1204d87d756d9
Kevin Murphy <kbmurph@gmail.com>
2021-05-09 10:59:38 -0500

    dt.py added

---
+++ dt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import datetime
+
+# datetime object containing current date and time
+now = datetime.datetime.now()
+
+# mm/dd/YY,H:M:S
+dt_string = now.strftime("%m/%d/%Y,%H:%M")
+print(dt_string)
